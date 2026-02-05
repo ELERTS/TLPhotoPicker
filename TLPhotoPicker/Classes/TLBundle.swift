6fc2fc09738853dc5560819fe2acc23432156ad8
Jared LaSante <jaredlasante@gmail.com>
2026-02-05 15:40:15 -0500

    Fixing bundle for Pods

--- TLPhotoPicker/Classes/TLBundle.swift
+++ TLPhotoPicker/Classes/TLBundle.swift
@@ -9,18 +9,13 @@
 import UIKit
 
 open class TLBundle {
-    public static var isPod: Bool = false
     open class func podBundleImage(named: String) -> UIImage? {
-        if( !isPod){
-            return UIImage(named: named, in: .module, compatibleWith: nil)
-        } else {
-            let podBundle = Bundle(for: TLBundle.self)
-            if let url = podBundle.url(forResource: "TLPhotoPickerController", withExtension: "bundle") {
-                let bundle = Bundle(url: url)
-                return UIImage(named: named, in: bundle, compatibleWith: nil)
-            }
-            return nil
+        let podBundle = Bundle(for: TLBundle.self)
+        if let url = podBundle.url(forResource: "TLPhotoPickerController", withExtension: "bundle") {
+            let bundle = Bundle(url: url)
+            return UIImage(named: named, in: bundle, compatibleWith: nil)
         }
+        return nil
     }
     
     class func bundle() -> Bundle {
